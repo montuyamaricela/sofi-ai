d266781fbfb9d5c73ec5538c5df30b3c7b1113ad
RAM-FRANCO <ramchristianfranco2@gmail.com>
2025-03-06 11:56:44 +0800

    updates

--- app/components/common/Container.tsx
+++ app/components/common/Container.tsx
@@ -15,7 +15,7 @@ export function Container({
       className={`relative ${className} ${id != "hero" ? "py-14" : ""}`}
     >
       <div
-        className={`container m-auto px-8 md:px-20 text-white flex ${
+        className={`container m-auto px-8 md:px-5 text-white flex ${
           id == "hero" ? "py-10 md:py-0" : "py-10 lg:py-20"
         } flex-col justify-center items-center`}
       >
