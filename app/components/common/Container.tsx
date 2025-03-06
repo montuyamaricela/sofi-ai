4612ab382fd446a0056d576a686390d0431effd7
RAM-FRANCO <ramchristianfranco2@gmail.com>
2025-03-06 09:43:30 +0800

    test

--- app/components/common/Container.tsx
+++ app/components/common/Container.tsx
@@ -12,7 +12,7 @@ export function Container({
   return (
     <div id={id} className={`relative ${className} `}>
       <div className='container m-auto px-8 md:px-5 text-white flex py-10 lg:py-20 flex-col justify-center items-center'>
-        {children} test
+        {children}
       </div>
     </div>
   );
