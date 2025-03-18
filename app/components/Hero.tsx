66159d3241f99d5b3ed757d0fb3c43f792a190a5
Maricel Montuya <montuyamaricela@gmail.com>
2025-03-18 14:58:55 +0800

    update hero banner

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,40 +1,40 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-"use client";
-import { Container } from "./common/Container";
-import { Button } from "./ui/button";
-import { MessageCircle } from "lucide-react";
-import Image from "next/image";
-import phoneImage from "@/public/images/phone-image.png";
-import { TypewriterEffect } from "./ui/typewriter";
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-import { animationVariant } from "@/utils/animation";
+'use client';
+import { Container } from './common/Container';
+import { Button } from './ui/button';
+import { MessageCircle } from 'lucide-react';
+import Image from 'next/image';
+import phoneImage from '@/public/images/phone-image.png';
+import { TypewriterEffect } from './ui/typewriter';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { animationVariant } from '@/utils/animation';
 
 export default function Hero() {
   const greetings = [
     {
-      text: "Hello Sissy!",
-      className: "text-primary-color",
+      text: 'Hello Sissy!',
+      className: 'text-primary-color',
     },
     {
-      text: "Maayong adlaw! Kumusta?",
-      className: "text-primary-color",
+      text: 'Maayong adlaw! Kumusta?',
+      className: 'text-primary-color',
     },
     {
-      text: "안녕하세요! 잘 지내세요?",
-      className: "text-primary-color",
+      text: '안녕하세요! 잘 지내세요?',
+      className: 'text-primary-color',
     },
     {
-      text: "こんにちは！お元気ですか？",
-      className: "text-primary-color",
+      text: 'こんにちは！お元気ですか？',
+      className: 'text-primary-color',
     },
     {
-      text: "Bonjour ! Comment ça va",
-      className: "text-primary-color",
+      text: 'Bonjour ! Comment ça va',
+      className: 'text-primary-color',
     },
     {
-      text: "Mustah knah? 😜",
-      className: "text-primary-color",
+      text: 'Mustah knah? 😜',
+      className: 'text-primary-color',
     },
   ];
 
@@ -68,7 +68,7 @@ export default function Hero() {
       {/* <motion.div initial='offscreen' whileInView='onscreen'> */}
       <div className='grid gap-10 md:gap-5 lg:gap-0 md:grid-cols-2 items-center'>
         <div className='space-y-5 lg:space-y-8'>
-          <motion.div variants={animationVariant("y", -20)}>
+          <motion.div variants={animationVariant('y', -20)}>
             <div className='flex items-center gap-1 text-primary-color'>
               <MessageCircle className='w-8 h-8' fill='currentColor' />
               <TypewriterEffect
@@ -77,23 +77,30 @@ export default function Hero() {
               />
             </div>
           </motion.div>
-          <motion.div variants={animationVariant("x", -20)}>
-            <h1 className='text-4xl xl:text-5xl font-bold'>
-              Unlock the{" "}
+          <motion.div variants={animationVariant('x', -20)}>
+            <h1 className='text-4xl xl:text-5xl font-bold capitalize'>
+              {/* Unlock the{" "}
               <span className='text-primary-color'>Full Potential</span> of AI
-              for Your Brand
+              for Your Brand */}
+              Unlock your{' '}
+              <span className='text-primary-color'>
+                brand&apos;s full potential
+              </span>{' '}
+              with AI that converts
             </h1>
           </motion.div>
-          <motion.div variants={animationVariant("y", 20)}>
-            {" "}
+          <motion.div variants={animationVariant('y', 20)}>
             <p className=' text-primary-grayText'>
-              Revolutionize your brand&apos;s customer experience with
+              {/* Revolutionize your brand&apos;s customer experience with
               intelligent, AI-powered automation. Boost efficiency, enhance
-              customer engagement, and scale effortlessly with SOFI AI.
-            </p>{" "}
+              customer engagement, and scale effortlessly with SOFI AI. */}
+              Scale the way you engage your customers with intuitive, AI-enabled
+              automation. Increase your online sales efficiency, enhance your
+              customer experience, and effortlessly sell more.
+            </p>
           </motion.div>
 
-          <motion.div variants={animationVariant("x", 20)}>
+          <motion.div variants={animationVariant('x', 20)}>
             <div className='flex gap-5 items-center'>
               <Button className='border-transparent hover:bg-white hover:text-primary-color border transition-colors duration-500 text-base px-8 py-6 rounded-sm uppercase font-bold bg-primary-color'>
                 Book Now
@@ -101,7 +108,7 @@ export default function Hero() {
             </div>
           </motion.div>
         </div>
-        <motion.div variants={animationVariant("x", 20)}>
+        <motion.div variants={animationVariant('x', 20)}>
           <div className='relative'>
             <Image
               src={phoneImage}
